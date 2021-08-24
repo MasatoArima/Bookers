53ebdb65f01dd823d30ba7d941f5996e6396ef41
MasatoArima <afoc.0707@gmail.com>
2021-08-24 02:40:31 +0000

    [add]フラッシュメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,7 +15,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to book_path(book.id)
+    redirect_to book_path(book.id) ,notice: "Book was successfully created."
   end
 
   def edit
@@ -23,10 +23,9 @@ class BooksController < ApplicationController
   end
 
   def update
-
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book.id)
+    redirect_to book_path(book.id) ,notice: "Book was successfully updated."
   end
 
   def destroy
